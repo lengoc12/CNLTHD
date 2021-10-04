0509fc8aa3698cdea0cf891c850083e93cdca055
lengoc12 <52289366+lengoc12@users.noreply.github.com>
2021-10-04 19:01:20 +0700

    Create urls.py

---
+++ etravel/etravel/urls.py
@@ -0,0 +1,32 @@
+
+import debug_toolbar
+from django.contrib import admin
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Travel API",
+        default_version='v1',
+        description='APIs for TravelApp',
+        contact=openapi.Contact(email='tgngoc12@gmail.com'),
+        license=openapi.License(name='Le Thi Ngoc'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+urlpatterns = [
+    path('', include('travel.urls')),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('admin/', admin.site.urls),
+    #path('__debug__/', include(debug_toolbar.urls)),
+]
